a399e37859ad99966afed7e72175dd35b7d76dfd
rshaw2327 <rshaw2327@gmail.com>
2025-06-26 18:14:30 +0100

    added controllers and routes to blog

--- controllers/blog.controllers.js
+++ controllers/blog.controllers.js
@@ -22,3 +22,62 @@ exports.createBlog=async(req,res,next)=>{
         blog
     })
 }
+
+exports.getAllBlogs=async(req,res,next)=> {
+    const blogs = await Blogs.find()
+    if(!blogs){
+    return res.status(404).json({
+        success:false,
+        message:"cannot find blogs"
+    })
+}
+return res.status(200).json({
+    success:true,
+    message:"products fetched successfully",
+    blogs
+})}
+
+exports.getASingleBlog=async(req,res,next)=>{
+    const id=req.params.id
+    const blogs = await Blogs.findById(id)
+    if(!blogs){
+        return res.status(404).json({
+            success:false,
+            message:"cannot find the blog"
+        })
+
+    }
+    return res.status(200).json({
+        success:true,
+        message:"blog fetched successfully",
+        blogs
+    })
+}
+
+exports.updateABlog=async(req,res,next)=>{
+    const id=req.params.id
+    const updatedData=req.body
+    let blogs = await Blogs.findById(id)
+    if(!blogs){
+        return res.status(404).json({
+            success:false,
+            message:"cannot find the blog while updating "
+        })
+
+    }
+    blogs = await Blogs.findByIdAndUpdate(id,updatedData,{
+        new:true,
+        runValidators:true
+    })
+    return res.status(201).json({
+        success:true,
+        message:"blog updated successfully",
+        blogs
+    })
+
+}
+
+exports.deleteBlog= async(req,res,next)=>{
+    
+
+}
--- routes/blog.route.js
+++ routes/blog.route.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { createBlog } = require("../controllers/blog.controllers");
+const { createBlog, getAllBlogs,getASingleBlog,updateABlog } = require("../controllers/blog.controllers");
 
-const router=express.Router()
+const router = express.Router();
 
-router.route("/create").post(createBlog)
+router.route("/create").post(createBlog);
+router.route("/getAllBlogs").get(getAllBlogs);
+router.route("/getASingleBlog/:id").get(getASingleBlog);
+router.route("/updateABlog/:id").put(updateABlog);
 
-
-module.exports=router
+module.exports = router;
